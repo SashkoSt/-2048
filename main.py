2de8acd535cd74a6ec0ca26ada1d4fddfeb99d2c
SashkoSt <oleksandrstefaniyk@gmail.com>
2023-12-23 10:48:54 +0200

    Вікно

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+# Code by @codePerfect7
+# Youtube : https://youtube.com/@codeperfect
+# Twitter : https://twitter.com/codePerfect7
+# Github : https://github.com/codePerfect7
+
 import pygame
-print("hello")
+from random import randint, choice
+import colors as c
+
+WIDTH, HEIGHT = 400, 500
+FPS = 30
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('2048 Game')
+
+def main():
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(FPS)
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main()
+    
